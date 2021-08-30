719a1a1bdc2a44331ecf152867fcfa93f85e4cdd
orosy <hanei7632@gmail.com>
2021-08-30 17:39:44 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main/Main';
+import Contents from './pages/Contents/Contents';
+import Profile from './pages/Profile/Profile';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/contents" component={Contents} />
+          <Route exact path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/pages/Contents/Aside/Aside.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Aside extends Component {
+  render() {
+    return <div>안녕하세요</div>;
+  }
+}
+
+export default Aside;
