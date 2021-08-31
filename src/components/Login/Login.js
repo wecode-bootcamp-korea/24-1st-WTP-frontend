7bac3a2301472db0df51174b6619a58038a3d689
orosy <hanei7632@gmail.com>
2021-08-31 21:10:25 +0900

    Add: Login 페이지 구현중

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,82 @@
 import React, { Component } from 'react';
+import './Login.scss';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      pw: '',
+    };
+  }
+
+  handleInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
-    return <div>안녕하세요</div>;
+    const { email, pw } = this.state;
+    const { handleInput } = this;
+    const checkPw = pw.length === 0 || pw.length >= 6;
+
+    return (
+      <div className="login">
+        <div className="login-container">
+          <div className="login-logo">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b8/%EC%99%93%EC%B1%A0_%EB%A1%9C%EA%B3%A0_2021.png"
+              alt="logo"
+            />
+          </div>
+          <div className="login-title">
+            <span>로그인</span>
+          </div>
+          <form className="login-form">
+            <div className="email-container">
+              <input
+                id="email"
+                name="email"
+                className="email"
+                placeholder="이메일"
+                type="text"
+                onChange={handleInput}
+              />
+              <i className={`fas fa-times-circle ${email ? 'none' : ''}`}></i>
+              <i
+                className={`fas fa-exclamation-circle ${email ? 'none' : ''}`}
+              ></i>
+            </div>
+            <div className="email-validation">
+              <p className={`email-caution ${email ? 'none' : ''}`}>
+                정확하지 않은 이메일입니다.
+              </p>
+            </div>
+            <div className="pw-container">
+              <input
+                id="pw"
+                name="pw"
+                className="password"
+                placeholder="비밀번호"
+                type="text"
+                onChange={handleInput}
+              />
+              <i className={`fas fa-times-circle ${checkPw ? 'none' : ''}`}></i>
+              <i className={`far fa-check-circle ${checkPw ? 'none' : ''}`}></i>
+            </div>
+            <div className="pw-validation">
+              <p className={`pw-caution ${checkPw ? 'none' : ''}`}>
+                비밀번호는 최소 6자리 이상이어야 합니다.
+              </p>
+            </div>
+            <button className="btn-login">로그인</button>
+          </form>
+          <p className="forget-pw">비밀번호를 잊어버리셨나요?</p>
+          <p className="have-account">
+            계정이 없으신가요?<span>회원가입</span>
+          </p>
+        </div>
+      </div>
+    );
   }
 }
