0d19e7cd8efc58c624a3d1684dfac075a0574f61
youngho <youngho8762@gamil.com>
2021-09-02 16:44:53 +0900

    Modify : CSS 일부 변경

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,41 +11,43 @@ export default class Footer extends Component {
           <span className="star-count"> ★ 626,921,264 개의 평가가 </span>
           쌓였어요.
         </div>
-        <div className="footer-bottom">
-          <div className="footer-left">
-            <div>
-              <a href="!#" className="right-bar right-bar-far">
-                서비스 이용약관
-              </a>
-              <a href="!#" className="right-bar right-bar-far">
-                개인정보 처리방침
-              </a>
-              <a href="!#" className="right-bar-far">
-                회사 안내
-              </a>
+        <div className="footer-bottom-container">
+          <div className="footer-bottom">
+            <div className="footer-left">
+              <div>
+                <a href="!#" className="right-bar right-bar-far">
+                  서비스 이용약관
+                </a>
+                <a href="!#" className="right-bar right-bar-far">
+                  개인정보 처리방침
+                </a>
+                <a href="!#" className="right-bar-far">
+                  회사 안내
+                </a>
+              </div>
+              <FooterList />
+              <div className="logo-div">
+                <img
+                  className="logo"
+                  alt="atchapedia"
+                  src="images/Atcha_Pedia_Logo_reversed.png"
+                />
+                © 2021 by ATCHA, Inc. All rights reserved.
+              </div>
             </div>
-            <FooterList />
-            <div className="logo-div">
-              <img
-                className="logo"
-                alt="atchapedia"
-                src="images/Atcha_Pedia_Logo_reversed.png"
-              />
-              © 2021 by ATCHA, Inc. All rights reserved.
-            </div>
-          </div>
-          <div className="footer-right">
-            <div className="select-language">한국어</div>
-            <div className="footer-right-icons">
-              <a href="!#">
-                <img alt="facebook" src="images/facebook.png" />
-              </a>
-              <a href="!#">
-                <img alt="twitter" src="images/twitter.png" />
-              </a>
-              <a href="!#">
-                <img alt="blogger" src="images/blogger.png" />
-              </a>
+            <div className="footer-right">
+              <div className="select-language">한국어</div>
+              <div className="footer-right-icons">
+                <a href="!#">
+                  <img alt="facebook" src="images/facebook.png" />
+                </a>
+                <a href="!#">
+                  <img alt="twitter" src="images/twitter.png" />
+                </a>
+                <a href="!#">
+                  <img alt="blogger" src="images/blogger.png" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
