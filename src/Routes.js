c1bc6715741a7b545c2ccd68f98930db92acbe21
youngho <youngho8762@gamil.com>
2021-09-03 16:23:59 +0900

    Add : 레이아웃 완료

--- src/Routes.js
+++ src/Routes.js
@@ -13,8 +13,7 @@ class Routes extends Component {
       <Router>
         <Nav />
         <Switch>
-          {/* <Route exact path="/" component={Main} /> */}
-          <Route exact path="/" component={Profile} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/contents" component={Contents} />
           <Route exact path="/profile" component={Profile} />
         </Switch>
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -6,16 +6,35 @@ export default class Profile extends Component {
     return (
       <div className="profile">
         <div className="profile-container">
-          <div className="profile-container-top">톱니바퀴~</div>
+          <div className="profile-container-top">
+            <div className="options-gradation">
+              <img
+                className="options"
+                alt="options"
+                src="/images/settings.png"
+              />
+            </div>
+          </div>
           <div className="profile-container-middle">
             <img className="user-picture" alt="user" src="images/user.png" />
-            <div className="profile-name">김영호</div>
-            <div className="profile-detail">프로필이 없습니다.</div>
-            <hr></hr>
-            <div className="analysis">취향분석</div>
-            <hr></hr>
+            <div className="profile-info">
+              <div className="profile-name">김영호</div>
+              <div className="profile-detail">프로필이 없습니다.</div>
+            </div>
+            <div className="analysis">
+              <img className="graph" alt="graph" src="images/bar-chart.png" />
+              <div className="analysis-text">취향분석</div>
+            </div>
           </div>
-          <div className="profile-container-bottom">영화</div>
+          <div className="profile-container-bottom">
+            <div className="bottom-top">
+              <div className="bottom-top-movie">영화</div>
+              <div className="bottom-top-star">★ 42</div>
+            </div>
+            <div className="bottom-bottom">
+              <div className="bottom-top-like">보고 싶어요 32</div>
+            </div>
+          </div>
         </div>
       </div>
     );
