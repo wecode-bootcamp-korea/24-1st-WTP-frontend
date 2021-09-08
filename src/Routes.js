ddb0224754b89505cca2212f44ae4d0dd6755e3e
youngho <youngho8762@gamil.com>
2021-09-08 19:12:31 +0900

    Modify : 라우트 일부 수정

--- src/Routes.js
+++ src/Routes.js
@@ -14,8 +14,7 @@ class Routes extends Component {
       <Router>
         <Nav />
         <Switch>
-          {/* <Route exact path="/" component={Main} /> */}
-          <Route exact path="/" component={MoviesCarousel} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/contents" component={Contents} />
           <Route exact path="/profile" component={Profile} />
         </Switch>
